e0b795744dd68d7a9f4ce8a45919f944014b5fd6
Dmytro Klimenko <dmytro_klimenko@epam.com>
2023-02-21 11:35:53 +0200

    Fix build error after v5.10.41 merge

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -66,6 +66,7 @@ EXPORT_SYMBOL(memstart_addr);
  * otherwise it is empty.
  */
 phys_addr_t arm64_dma_phys_limit __ro_after_init;
+static phys_addr_t arm64_dma32_phys_limit __ro_after_init;
 
 #ifdef CONFIG_KEXEC_CORE
 /*
